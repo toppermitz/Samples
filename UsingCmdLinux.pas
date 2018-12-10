cd64c78c615d09c74ea47f74d50a70ffb59998dd
anderson <desenvolvimento@serverinfo.com.br>
2018-12-10 13:42:42 -0200

    Initial Samples

---
+++ UsingCmdLinux.pas
@@ -0,0 +1,16 @@
+procedure TCPUInfo.GetModel;
+var
+  Cmd : String;
+begin
+  try
+    Cmd := 'cat /proc/cpuinfo | grep -i "^model name" | awk -F": " ''{print $2}'' | head -1 | sed ''s/ \+/ /g''';
+    TLinuxUtils.RunCommand(Cmd,FModel);
+  except
+    on E:Exception do
+    begin
+    {$IFDEF DEBUG}
+      TLinuxUtils.LogError('TCPUInfo.GetModel - ' + E.Message);
+    {$ENDIF}
+    end;
+  end;
+end;
